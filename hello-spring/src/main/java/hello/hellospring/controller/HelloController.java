4e567aa741accca0b032ca27ab6951bf3f90e831
yunjeong <stac_cato@naver.com>
2023-05-10 21:56:47 +0900

    [입문] 섹션 1. 프로젝트 환경설정

---
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,14 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello"; // 이건 hello.html 의미하는 것임
+    }
+}
