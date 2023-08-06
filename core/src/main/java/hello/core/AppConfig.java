a234dea6676db1b09adc54a15e195c0aedf05ade
yunjeong <stac_cato@naver.com>
2023-08-06 19:03:53 +0900

    [스프링 핵심 원리 기본편] 스프링 컨테이너와 스프링 빈

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -8,21 +8,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig { // 구체적인 객체를 주입한다
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     } // 생성자 주입
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
